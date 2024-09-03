7bc9eb23046f11ccf87293ff1cf48686540e7d03
Contenedor01 <112082639+Contenedor01@users.noreply.github.com>
2024-09-03 11:30:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyxhNHdDvQuBQIOpKsZJB-sTIlpHLMBRASB7qlgjsPzEEn4zjB2idrFGhj21dtjsQs7Ag/exec";
